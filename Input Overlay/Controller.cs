9b0f3e1ef1f4235892788545e2ded8efe5ebf428
Aaron Thompson <aaronjthompson98@gmail.com>
2019-02-07 00:19:31 +0000

    First Commit
    Initial Commit. This is far from finished and requires a complete Low Level Input Hook rewrite to be functional with games. Until I learn .Net and windows functions better (hopefully very soon) this project is on a very temporary hiatus.

---
+++ Input Overlay/Controller.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using XInput.Wrapper;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Input_Overlay
+{
+    class Controller
+    {
+        public X.Gamepad gamepad = null;
+        private Point leftStickPos = new Point(180, 116);
+        private Point rightStickPos = new Point(435, 214);
+        private Point XPos = new Point(492, 136);
+        private Point YPos = new Point(538, 92);
+        private Point APos = new Point(538, 180);
+        private Point BPos = new Point(582, 136);
+        private Point LeftBumperPos = new Point(164, 35);
+        private Point RightBumperPos = new Point(480, 35);
+        private Point LeftTriggerPos = new Point(132, 45);
+        private Point RightTriggerPos = new Point(578, 45);
+        private Point UpDpadPos = new Point(288, 220);
+        private Point DownDpadPos = new Point(288, 283);
+        private Point RightDpadPos = new Point(320, 250);
+        private Point LeftDpadPos = new Point(256, 250);
+        private float stickRadius = 30f;
+        public Controller(int id)
+        {
+            if (X.IsAvailable)
+            {
+                gamepad = GetGamepad(id);
+                gamepad.LStick_DeadZone = 2000;
+                X.StartPolling(gamepad);
+            }
+        }
+        public Controller(X.Gamepad gp)
+        {
+            if (X.IsAvailable)
+            {
+                gamepad = gp;
+                gamepad.LStick_DeadZone = 2000;
+                X.StartPolling(gamepad);
+            }
+        }
+        private X.Gamepad GetGamepad(int id)
+        {
+            X.Gamepad gp = null;
+            switch (id)
+            {
+                case 1:
+                    gp = X.Gamepad_1;
+                    break;
+                case 2:
+                    gp = X.Gamepad_2;
+                    break;
+                case 3:
+                    gp = X.Gamepad_3;
+                    break;
+                case 4:
+                    gp = X.Gamepad_4;
+                    break;
+                default:
+                    gp = X.Gamepad_1;
+                    break;
+            }
+            return gp;
+        }
+        public void Paint(float X, float Y,PaintEventArgs e)
+        {
+            Image leftTriggerImage = gamepad.LTrigger_N > 0.1f ? Properties.Resources.LeftTrigger_Pressed : Properties.Resources.LeftTrigger;
+            Image rightTriggerImage = gamepad.RTrigger_N > 0.1f ? Properties.Resources.RightTrigger_Pressed : Properties.Resources.RightTrigger;
+            e.Graphics.DrawImageUnscaled(leftTriggerImage, LeftTriggerPos);
+            e.Graphics.DrawImageUnscaled(rightTriggerImage, RightTriggerPos);
+
+            Image leftBumperImage = gamepad.LBumper_down ? Properties.Resources.LeftBumper_Pressed : Properties.Resources.LeftBumper;
+            Image rightBumperImage = gamepad.RBumper_down ? Properties.Resources.RightBumper_Pressed : Properties.Resources.RightBumper;
+            e.Graphics.DrawImageUnscaled(leftBumperImage, LeftBumperPos);
+            e.Graphics.DrawImageUnscaled(rightBumperImage, RightBumperPos);
+
+            e.Graphics.DrawImageUnscaled(Properties.Resources.Controller, 0, 0);
+
+            X.PointF LStick = gamepad.LStick_N;
+            X.PointF RStick = gamepad.RStick_N;
+            PaintStick(leftStickPos, LStick, e);
+            PaintStick(rightStickPos, RStick, e);
+            PaintButton(XPos, gamepad.X_down, e);
+            PaintButton(YPos, gamepad.Y_down, e);
+            PaintButton(BPos, gamepad.B_down, e);
+            PaintButton(APos, gamepad.A_down, e);
+            PaintDPADButton(UpDpadPos, RotateFlipType.RotateNoneFlipNone, gamepad.Dpad_Up_down, e);
+            PaintDPADButton(DownDpadPos, RotateFlipType.Rotate180FlipNone, gamepad.Dpad_Down_down, e);
+            PaintDPADButton(RightDpadPos, RotateFlipType.Rotate90FlipNone, gamepad.Dpad_Right_down, e);
+            PaintDPADButton(LeftDpadPos, RotateFlipType.Rotate270FlipNone, gamepad.Dpad_Left_down, e);
+        }
+        private void PaintStick(Point point, X.PointF stick, PaintEventArgs e)
+        {
+            bool stickChanged = (bool)(stick.X + stick.Y != 0);
+            Image stickImage = stickChanged ? Properties.Resources.Stick_Pressed : Properties.Resources.Stick;
+            Point stickFinal = new Point(point.X, point.Y);
+            stickFinal.Offset((int)(stick.X * stickRadius), (int)(stick.Y * -stickRadius));
+            e.Graphics.DrawImageUnscaled(stickImage, stickFinal);
+
+        }
+        private void PaintButton(Point point, bool pressed, PaintEventArgs e) {
+            Image img = pressed ? Properties.Resources.Button_Pressed : Properties.Resources.Button;
+            e.Graphics.DrawImageUnscaled(img, point);
+        }
+        private void PaintDPADButton(Point point, RotateFlipType rotate, bool pressed, PaintEventArgs e)
+        {
+            Image img = pressed ? Properties.Resources.DPad_Pressed : Properties.Resources.DPad;
+            img.RotateFlip(rotate);
+            e.Graphics.DrawImageUnscaled(img, point);
+        }
+        public void Stop()
+        {
+            X.StopPolling();
+        }
+    }
+}
---
+++ Input Overlay/Mouse.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gma.System.MouseKeyHook;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using Input = System.Windows.Input;
+namespace Input_Overlay
+{
+    class Mouse
+    {
+        private IMouseEvents m_GlobalHook;
+        private FixedSizedQueue<MouseFrame> mouseBuffer;
+        private Stopwatch timer;
+        private MouseFrame lastMouseFrame;
+        private MouseSpeed currentSpeed;
+        private Point pos;
+        public Mouse(Point pos,IMouseEvents km)
+        {
+            mouseBuffer = new FixedSizedQueue<MouseFrame>(10);
+            timer = Stopwatch.StartNew();
+            m_GlobalHook = km;
+            m_GlobalHook.MouseMove += M_GlobalHook_MouseMove;
+            this.pos = pos;
+        }
+
+        private void M_GlobalHook_MouseMove(object sender, MouseEventArgs e)
+        {
+            AddSpeedFrame(Cursor.Position.X, Cursor.Position.Y);
+        }
+        private void AddSpeedFrame(int x, int y)
+        {
+            float speedX = 0;
+            float speedY = 0;
+            MouseFrame currentFrame = new MouseFrame(x, y, timer.ElapsedMilliseconds);
+            if (lastMouseFrame == null)
+            {
+                lastMouseFrame = currentFrame;
+                return;
+            }
+            long dT = currentFrame.TimeStamp - lastMouseFrame.TimeStamp;
+            if (dT > 0) {
+                speedX = (currentFrame.X - lastMouseFrame.X) / dT;
+                speedY = (currentFrame.Y - lastMouseFrame.Y) / dT;
+                Console.WriteLine("dX: \t{0} dY: \t{1}", speedX, speedY);
+            }
+            currentSpeed = new MouseSpeed(speedX, speedY);
+            lastMouseFrame = currentFrame;
+        }
+        private void OutputFlick()
+        {
+            MouseFrame firstFrame = mouseBuffer.First<MouseFrame>();
+            MouseFrame lastFrame = mouseBuffer.Last<MouseFrame>();
+            if (firstFrame == null || lastFrame == null || firstFrame == lastFrame)
+                return;
+            int dX = lastFrame.X - firstFrame.X;
+            int dY = lastFrame.Y - firstFrame.Y;
+            long dT = lastFrame.TimeStamp - firstFrame.TimeStamp;
+            float totalMovement;
+            if (dT > 0)
+            {
+                totalMovement = (Math.Abs(dX) + Math.Abs(dY)) / dT;
+            } else
+            {
+                totalMovement = 5;
+            }
+            Console.WriteLine("Movement: \t{0}", totalMovement);
+        }
+        public void Paint(PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pens.White, this.pos, new PointF(pos.X + (currentSpeed.SpeedX * 10), pos.Y + (currentSpeed.SpeedY * 10)));
+        }
+    }
+
+    class MouseFrame
+    {
+        public int X;
+        public int Y;
+        public long TimeStamp;
+        public MouseFrame(int x, int y, long time)
+        {
+            this.X = x;
+            this.Y = y;
+            this.TimeStamp = time;
+        }
+    }
+    class MouseSpeed
+    {
+        public float SpeedX;
+        public float SpeedY;
+        public MouseSpeed(float x, float y)
+        {
+            this.SpeedX = x;
+            this.SpeedY = y;
+        }
+    }
+
+    public class FixedSizedQueue<T> : ConcurrentQueue<T>
+    {
+        private readonly object syncObject = new object();
+
+        public int Size { get; private set; }
+
+        public FixedSizedQueue(int size)
+        {
+            Size = size;
+        }
+
+        public new void Enqueue(T obj)
+        {
+            base.Enqueue(obj);
+            lock (syncObject)
+            {
+                while (base.Count > Size)
+                {
+                    T outObj;
+                    base.TryDequeue(out outObj);
+                }
+            }
+        }
+    }
+}
