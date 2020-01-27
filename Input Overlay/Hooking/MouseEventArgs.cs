3a9c19de884fef04998e3854546b08461e9508de
AaronJThompson <aaronjthompson98@gmail.com>
2020-01-27 20:13:31 +0000

    Add hooking from old proj

---
+++ Input Overlay/Hooking/MouseEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Overlay.Hooking
+{
+    public class MouseEventArgs : EventArgs
+    {
+        public MouseEventArgs(MouseData data)
+        {
+            Data = data;
+        }
+
+        public MouseData Data { get; }
+    }
+}
